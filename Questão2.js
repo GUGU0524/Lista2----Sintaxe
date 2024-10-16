d3d37085848cfa2555938fc0645eb00096c5b80e
GUGU0524 <gustavogomesdacunha05@gmail.com>
2024-10-16 12:06:33 -0300

    Update Questão2.js

--- Questão2.js
+++ Questão2.js
@@ -2,9 +2,9 @@ function ordenarPorIdade(pessoas) {
     return pessoas.sort((a, b) => a.idade - b.idade);
 }
 const pessoas = [
-    { nome: 'Helena', idade: 4 },
-    { nome: 'Ayla', idade: 2 },
-    { nome: 'Martin', idade: 1 },
+    { nome: 'Caio Jorge', idade: 13 },
+    { nome: 'Dirk', idade: 19 },
+    { nome: 'Renan', idade: 17 },
 ];
 const pessoasOrdenadas = ordenarPorIdade(pessoas);
 console.log(pessoasOrdenadas);
